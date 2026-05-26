e96fee01bc2c5eca675897e753c7e65b81000cd0
sankarboya <boyasankar42@gmail.com>
2026-05-26 14:34:07 +0530

    updated file

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,4 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [react()],
-})
-
-import { defineConfig } from 'vite'
 
 export default defineConfig({
   preview: {
