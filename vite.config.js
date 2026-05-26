94912a35a353dcdc3b9dd4bcf6e2a0b4d8fbb456
sankarboya <boyasankar42@gmail.com>
2026-05-26 14:37:59 +0530

    updated file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   preview: {
     host: true,
-    allowedHosts: ['portpoflio-3.onrender.com']
+    allowedHosts: ['preview.allowedHosts']
   }
 })
